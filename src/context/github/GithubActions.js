5c2dd3f186c4d34369d5a222b260ef2047c69110
Jonathon <cisneros.jonathon@gmail.com>
2022-05-11 09:41:02 -0700

    adjusted search params

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -6,14 +6,12 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 const github = axios.create({
   baseURL: GITHUB_URL,
   headers: { Authorization: `token: ${GITHUB_TOKEN }` }
-})
+});
 
 /***** Get users via search *****/
 export const searchUsers = async ( text ) => {
 
-  const params = new URLSearchParams({
-    q: text
-  });
+  const params = new URLSearchParams({ q: text });
 
   const response = await github.get( `/search/users?${ params }` );
   return response.data.items;
@@ -22,9 +20,16 @@ export const searchUsers = async ( text ) => {
 
 /***** Get user and repos *****/
 export const getUserAndRepos = async ( login ) => {
+
+  // only display latest 10 repos
+  const params = new URLSearchParams({
+		sort: 'created',
+		per_page: 10,
+  });
+
   const [ user, repos ] = await Promise.all([
     github.get(`/users/${ login }`),
-    github.get(`/users/${ login }/repos`)
+    github.get(`/users/${ login }/repos?${ params }`),
   ]);
 
   return { user: user.data, repos: repos.data }
