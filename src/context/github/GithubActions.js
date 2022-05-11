08be99d59c6f40a4f06601d5f2903b2ffc15f5a7
Jonathon <cisneros.jonathon@gmail.com>
2022-05-11 05:49:48 -0700

    add Netlify functions

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,31 +1,18 @@
 import axios from 'axios';
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
-const github = axios.create({
-  baseURL: GITHUB_URL,
-  headers: { Authorization: `token: ${GITHUB_TOKEN }` }
-})
-
 /***** Get users via search *****/
 export const searchUsers = async ( text ) => {
+  const params = new URLSearchParams({ q: text });
 
-  const params = new URLSearchParams({
-    q: text
-  });
-
-  const response = await github.get( `/search/users?${ params }` );
-  return response.data.items;
+  const response = await axios.get( `/api/searchUsers?${ params }` );
+  return response.data;
 }
 
 
 /***** Get user and repos *****/
 export const getUserAndRepos = async ( login ) => {
-  const [ user, repos ] = await Promise.all([
-    github.get(`/users/${ login }`),
-    github.get(`/users/${ login }/repos`)
-  ]);
+	const params = new URLSearchParams({ q: login });
 
-  return { user: user.data, repos: repos.data }
+  const reponse = await axios.get(`/api/getUserAndRepos?${ params }`);
+  return response.data;
 }
