cbccf8efdba60f8096bee355cf70ff27c85a0d91
Jonathon Cisneros <38201359+JonathonCisneros@users.noreply.github.com>
2022-05-11 18:43:51 -0700

    debugging getUsersAndRepos

--- functions/getUsersAndRepos.js
+++ functions/getUsersAndRepos.js
@@ -1,15 +1,16 @@
 const github = require('./utils/github');
 
 exports.handler = async function ( event ) {
+  console.log(event);
   const login = event.queryStringParameters.q;
-  // const params = new URLSearchParams({
-	// 	sort: 'created',
-	// 	per_page: 10,
-  // });
+  const params = new URLSearchParams({
+		sort: 'created',
+		per_page: 10,
+  });
 
   const [ user, repos ] = await Promise.all([
-    github.get(`/users/${ login }`);
-    // github.get(`/users/${ login }/repos?${ params }`),
+    github.get(`/users/${ login }`),
+    github.get(`/users/${ login }/repos?${ params }`),
   ]);
 
   return {
--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 /***** Get users via search *****/
 export const searchUsers = async ( text ) => {
-  const params = new URLSearchParams({ q: text });
+  const params = new URLSearchParams({ q: text, });
 
   const response = await axios.get( `/api/searchUsers?${ params }` );
   return response.data;
