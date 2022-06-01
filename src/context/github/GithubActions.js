bee9a41b900b02ab24deb93762d818f8512d3be3
Jonathon Cisneros <38201359+JonathonCisneros@users.noreply.github.com>
2022-05-31 21:39:36 -0700

    .env ignored

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -5,32 +5,29 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 const github = axios.create({
   baseURL: GITHUB_URL,
-  headers: { Authorization: `token: ${GITHUB_TOKEN }` }
+  headers: { Authorization: `token: ${GITHUB_TOKEN}` },
 });
 
 /***** Get users via search *****/
-export const searchUsers = async ( text ) => {
-
+export const searchUsers = async (text) => {
   const params = new URLSearchParams({ q: text });
 
-  const response = await github.get( `/search/users?${ params }` );
+  const response = await github.get(`/search/users?${params}`);
   return response.data.items;
-}
-
+};
 
 /***** Get user and repos *****/
-export const getUserAndRepos = async ( login ) => {
-
+export const getUserAndRepos = async (login) => {
   // only display latest 10 repos
   const params = new URLSearchParams({
-		sort: 'created',
-		per_page: 10,
+    sort: 'created',
+    per_page: 10,
   });
 
-  const [ user, repos ] = await Promise.all([
-    github.get(`/users/${ login }`),
-    github.get(`/users/${ login }/repos?${ params }`),
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos?${params}`),
   ]);
 
-  return { user: user.data, repos: repos.data }
-}
+  return { user: user.data, repos: repos.data };
+};
